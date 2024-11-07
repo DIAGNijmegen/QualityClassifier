467852582dd60c25e83da9846ff9a177a22d50e9
Tiago Filipe Coelho <tiago.coelho@radboudumc.nl>
2024-11-07 17:50:39 +0100

    Picai inference and local ensemble

---
+++ getting_data/9_inference_PI-CAI_val/inference_pimed_translation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --qos=low
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=7-00:00:00
+#SBATCH --no-container-remap-root
+#SBATCH --nodelist=dlc-articuno,dlc-lugia,dlc-moltres,dlc-nidoking,dlc-tyranitar,dlc-zapdos
+#SBATCH --container-mounts=/data/pelvis/projects/tiago/DWI_IQA/validation_procanceri_100:/dataset:ro,/data/pelvis/projects/tiago/DWI_IQA/validation_procanceri_100/Pimed:/results
+#SBATCH --container-image="doduo1.umcn.nl#joeran/picai_pimed_inference_docker_batched:v1.5"
+
+python batch_inference.py \
+    --t2w_input={patient_id}/{subject_id}_t2w.mha \
+    --adc_input={patient_id}/{subject_id}_adc.mha \
+    --hbv_input={patient_id}/{subject_id}_hbv.mha \
+    --subject_list=/dataset/subject_list.txt
