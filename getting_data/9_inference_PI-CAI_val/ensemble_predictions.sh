d021ab67e7907bc85337bb0f5019e0a4e9e79569
Tiago Filipe Coelho <tiago.coelho@radboudumc.nl>
2025-04-09 12:00:01 +0200

    removing data curation scripts

--- getting_data/9_inference_PI-CAI_val/ensemble_predictions.sh
+++
@@ -1,9 +0,0 @@
-python 2023-12-20-ensemble-PI-CAI-algorithms.py \
-    --prediction_dirs \
-        /Volumes/pelvis/projects/tiago/DWI_IQA/validation_procanceri_100/Heviai \
-        /Volumes/pelvis/projects/tiago/DWI_IQA/validation_procanceri_100/BDAV_Y \
-        /Volumes/pelvis/projects/tiago/DWI_IQA/validation_procanceri_100/Pimed \
-        /Volumes/pelvis/projects/tiago/DWI_IQA/validation_procanceri_100/DataScientX \
-        /Volumes/pelvis/projects/tiago/DWI_IQA/validation_procanceri_100/Swangeese \
-    --output_dir \
-        /Volumes/pelvis/projects/tiago/DWI_IQA/validation_procanceri_100/ensemble5
--- getting_data/utils/delete._.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-from utils import delete_dot_underscore_files
-
-folder_path = Path('/Volumes/pelvis/projects/tiago/DWI_IQA/data')
-
-delete_dot_underscore_files(folder_path)
